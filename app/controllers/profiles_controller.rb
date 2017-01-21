7a4ea6a7bfc8a7933ea75239f5b38e6388c6a7d5
Nick Grenwalt <ngrenwalt@gmail.com>
2017-01-21 15:43:45 +0000

    Added users controller and show view

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -20,7 +20,7 @@ class ProfilesController < ApplicationController
     end
   end
   
-  # White list fields that should be accepted by the form
+  # Whitelist fields that should be accepted by the form for security
   private
     def profile_params
       params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,7 @@
+class UsersController < ApplicationController
+  
+  # GET request to /users/:id
+  def show
+    @user = User.find( params[:id] )
+  end
+end
